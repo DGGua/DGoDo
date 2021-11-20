449276a2080bbb80fc648ad9bdcf7e3297a755e2
DGGua <41000029+DGGua@users.noreply.github.com>
2021-11-21 06:10:00 +0800

    [feat]: panel style

--- src/pages/Panel/Panel.tsx
+++ src/pages/Panel/Panel.tsx
@@ -6,20 +6,22 @@ export function Panel(props: any) {
         <div className="div-panel">
             <div className="hiuser">
                 {/* <img src={nihao } alt></img> */}
-                <p>Hi，</p>
-                <p>Unier</p>
+                <p id="hi">Hi，</p>
+                <p id="username">Unier</p>
             </div>
+            <ul className="choice">
 
-            {[0, 1, 2].map(
-                (index) => {
-                    return <li
-                        id={props.checkout === index ? "checked" : ""}
-                        onClick={() => { props.setCheckout(index) }}
-                    >
-                        {list[index]}
-                    </li>
-                }
-            )}
+                {[0, 1, 2].map(
+                    (index) => {
+                        return <li
+                            id={props.checkout === index ? "checked" : ""}
+                            onClick={() => { props.setCheckout(index) }}
+                        >
+                            {list[index]}
+                        </li>
+                    }
+                )}
+            </ul>
         </div >
     )
 }
