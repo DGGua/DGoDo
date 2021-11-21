201327686258c85fbc361e3ed86ced9aceb92fe3
ldz <DG_Gua@foxmail.com>
2021-11-21 23:18:03 +0800

    [fix]: default value problem for DDLmodel

--- src/model/DDL.ts
+++ src/model/DDL.ts
@@ -8,11 +8,11 @@ export class DDL {
     active: boolean;
 
     // 创建一个DDL，id由localStorage已经存储的内容决定
-    constructor(content?: string, time?: Dayjs, active?: boolean) {
-        let DDLs = getDDLs();
-        this.id = DDLs.length > 0 ? DDLs.pop().id + 1 : 0
-        this.content = content || "";
-        this.time = time || new Dayjs();
-        this.active = active || true;
+    constructor(content: string = "", time: Dayjs = new Dayjs(), active: boolean = true) {
+        let ddls = getDDLs();
+        this.id = (ddls.pop()?.id ?? 0) + 1
+        this.content = content;
+        this.time = time;
+        this.active = active;
     }
 }
