452842edc8e0fc4bdd3bb639d87eccee7dee56a4
DGGua <41000029+DGGua@users.noreply.github.com>
2021-11-21 08:09:45 +0800

    [feat]: style of additem

--- src/components/AddItem.tsx
+++ src/components/AddItem.tsx
@@ -1,9 +1,12 @@
+import './AddItem.scss'
+import inputcircle from "../static/inputcircle.svg"
+import inputconfirm from "../static/inputconfirm.svg"
 export default function AddItem(props: any) {
     return (
         <div className="div-additem">
-            {/* <img></img> */}
-            <input />
-            {/* <img></img> */}
+            <img src={inputcircle}></img>
+            <input placeholder="添加你的日程" />
+            <img src={inputconfirm}></img>
         </div>
     )
 }
