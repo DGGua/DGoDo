833f0ca7924e6ea82e7c2b99716e4718bffdcfa2
ldz <DG_Gua@foxmail.com>
2021-11-23 15:48:29 +0800

    [feat]: 实现截止时间的显示

--- src/components/AddItem.tsx
+++ src/components/AddItem.tsx
@@ -3,14 +3,21 @@ import inputcircle from "../static/inputcircle.svg"
 import inputconfirm from "../static/inputconfirm.svg"
 import { useState } from 'react'
 import { DDL } from '../model/DDL'
+import dayjs from 'dayjs'
 export default function AddItem(props: any) {
 
     const [content, setContent] = useState("")
+    const [dayAfter, setDayAfter] = useState(0)
+    // handle the day change
+    const onChangeDate = (event: any) => {
+        setDayAfter(event.target.value);
+    }
 
     const onClickAdd = () => {
-        props.onClickAdd(new DDL(content))
+        props.onClickAdd(new DDL(content, dayjs().add(dayAfter, 'd')))
         setContent("")
     }
+
     return (
         <div className="div-additem">
 
@@ -22,11 +29,17 @@ export default function AddItem(props: any) {
                     setContent(event.target.value)
                 }}
                 onKeyPress={(event) => {
+                    // TODO: try something latest
                     if (event.which === 13) onClickAdd()
                 }}
             />
+            <select onChange={onChangeDate}>
+                <option value={0}>今天</option>
+                <option value={1}>明天</option>
+                <option value={7}>下周</option>
+            </select>
             <img src={inputconfirm}></img>
 
-        </div>
+        </div >
     )
 }
