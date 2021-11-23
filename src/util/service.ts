b3060852a2714c3465aa39b1f141fb52972e1ed8
ldz <DG_Gua@foxmail.com>
2021-11-23 10:28:59 +0800

    [feat]: add storage services at localstorage

---
+++ src/util/service.ts
@@ -0,0 +1,35 @@
+import { Dayjs } from "dayjs";
+import { DDL } from "../model/DDL";
+
+export const DDLService = {
+    getDDLs: getDDLs,
+    addDDL: addDDL,
+    compeleteDDL: compeleteDDL
+}
+
+function getDDLs(): DDL[] {
+
+    let DDLs = JSON.parse(localStorage.getItem("DDLs") ?? "[]")
+    console.log(DDLs)
+    return DDLs;
+}
+
+function setDDLs(DDLs: DDL[]) {
+    localStorage.setItem("DDLs", JSON.stringify(DDLs))
+}
+
+function addDDL(aDDL: DDL) {
+    let DDLs = getDDLs()
+    DDLs.push(aDDL)
+    setDDLs(DDLs)
+}
+
+function compeleteDDL(id: number) {
+    let DDLs = getDDLs()
+    DDLs.forEach(DDL => {
+        if (DDL.id === id) {
+            DDL.active = false
+        }
+    })
+    setDDLs(DDLs);
+}
