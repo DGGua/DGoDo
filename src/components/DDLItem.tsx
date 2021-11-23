ff8cc66500662a9e72cd4804ecbaaa6d214d667a
ldz <DG_Gua@foxmail.com>
2021-11-23 10:31:26 +0800

    [refactor]: changed display models temporarily

--- src/components/DDLItem.tsx
+++ src/components/DDLItem.tsx
@@ -1,12 +1,17 @@
 import './DDLItem.scss'
 import circle from '../static/circle.svg'
 import dots from '../static/dots.svg'
+import { DDL } from '../model/DDL'
+import { DDLService } from '../util/service'
 export default function DDLItem(props: any) {
+
+    const item: DDL = props.item
+    const onClickComplete = props.onClickComplete
     return (
         <div className="div-ddlitem">
-            <img src={circle}></img>
-            <p className="description">{props.item.description}</p>
-            <p className="date">{props.item.time.format('MM-DD')}</p>
+            <img src={circle} onClick={onClickComplete}></img>
+            <p className="description">{item.content}</p>
+            <p className="date">{item.time}</p>
             <img src={dots}></img>
         </div>
     )
