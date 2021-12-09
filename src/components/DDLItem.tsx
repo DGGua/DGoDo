1834210fd2e14b226318b259c496d990878c2e55
ldz <DG_Gua@foxmail.com>
2021-12-09 14:25:21 +0800

    [fix]: 修改时间显示问题

--- src/components/DDLItem.tsx
+++ src/components/DDLItem.tsx
@@ -3,6 +3,7 @@ import circle from '../static/circle.svg'
 import dots from '../static/dots.svg'
 import { DDL } from '../model/DDL'
 import { DDLService } from '../util/service'
+import dayjs from 'dayjs'
 export default function DDLItem(props: any) {
 
     const item: DDL = props.item
@@ -11,7 +12,7 @@ export default function DDLItem(props: any) {
         <div className="div-ddlitem">
             <img src={circle} onClick={onClickComplete}></img>
             <p className="description">{item.content}</p>
-            <p className="date">{item.time}</p>
+            <p className="date" style={{ color: dayjs(item.time).isBefore(dayjs(), 'day') ? "red" : "white" }}>{dayjs(item.time).format('MM-DD HH:mm')}</p>
             <img src={dots}></img>
         </div>
     )
