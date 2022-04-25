11309193b9dfb76d3e89468717aca8883b15c53a
DG_Gua <DG_Gua@foxmail.com>
2022-04-25 19:40:26 +0800

    fix type error

--- src/util/service.ts
+++ src/util/service.ts
@@ -3,60 +3,72 @@ import { DDL } from "../model/DDL";
 import axios from "axios";
 import { parse } from "path";
 
-interface  responseDate {
-    Status: number;
-    Msg: string;
-    Data: string;
+interface ResponseData<T> {
+  Status: number;
+  Msg: string;
+  Data: T;
 }
 
-const baseUrl = "http://127.0.0.1:4523/mock/468343"
+interface Task {
+  id: number;
+  name: string;
+  content: string;
+  create_time: string;
+  expect_time: string;
+  finish_time: string;
+  status: number;
+  root_id: string;
+  root_form: number;
+  form: number;
+}
+
+const baseUrl = "http://127.0.0.1:4523/mock/468343";
 
 export const DDLService = {
-    getDDLs: getDDLs,
-    addDDL: addDDL,
-    // compeleteDDL: compeleteDDL
-}
+  getDDLs: getDDLs,
+  addDDL: addDDL,
+  // compeleteDDL: compeleteDDL
+};
 
 const service = {
-    task:{
-        taskGet:() => {
-            return axios({
-                baseURL: baseUrl,
-                url: "/task/list",
-                method: "POST",
-            })
+  task: {
+    taskGet: () => {
+      return axios.post<ResponseData<Task[]>>(baseUrl + "/task/list");
+    },
+
+    taskCreate: (
+      content: string,
+      expect_time: string,
+      root_id: string = "",
+      text: string = "",
+      root_form: number = 0,
+      form: number = 2
+    ) => {
+      return axios({
+        baseURL: baseUrl,
+        url: "/task/create",
+        method: "POST",
+        data: {
+          content: content,
+          expect_time: expect_time,
+          text: text,
+          root_id: root_id,
+          root_form: root_form,
+          form: form,
         },
-        
-        taskCreate:(content:string, expect_time:string,  root_id:string="", text:string="",root_form:number=0, form: number=2) => {
-            return axios({
-                baseURL:baseUrl,
-                url:"/task/create",
-                method:"POST",
-                data:{
-                    content:content,
-                    expect_time:expect_time,
-                    text:text,
-                    root_id:root_id,
-                    root_form:root_form,
-                    form:form
-                }
-            })
-        }
-    }
-}
+      });
+    },
+  },
+};
 
 async function getDDLs() {
+  // let DDLs = JSON.parse(localStorage.getItem("DDLs") ?? "[]")
+  let DDLs;
+  let response = await service.task.taskGet();
+  let responseData = response.data;
+  DDLs = responseData.Data && JSON.stringify(responseData.Data);
 
-    // let DDLs = JSON.parse(localStorage.getItem("DDLs") ?? "[]")
-    let DDLs
-    let response = await service.task.taskGet()
-    let responseData:responseDate = response.data
-    console.log(response.data)
-    console.log(responseData.Data)
-    console.log(typeof(responseData.Data))
-    DDLs = responseData.Data && JSON.stringify(responseData.Data)
-
-    return DDLs
+  return DDLs;
 }
 
 // function setDDLs(DDLs: DDL[]) {
@@ -64,13 +76,12 @@ async function getDDLs() {
 // }
 
 function addDDL(aDDL: DDL) {
-    // let DDLs = getDDLs()
-    // DDLs.push(aDDL)
-    // setDDLs(DDLs)
-    const content = aDDL.content
-    const time: string = aDDL.time.toString()
-    service.task.taskCreate(content, time)
-
+  // let DDLs = getDDLs()
+  // DDLs.push(aDDL)
+  // setDDLs(DDLs)
+  const content = aDDL.content;
+  const time: string = aDDL.time.toString();
+  service.task.taskCreate(content, time);
 }
 
 // function compeleteDDL(id: number) {
